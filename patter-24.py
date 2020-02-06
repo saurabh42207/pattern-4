f59dd22cb3b41c277b0b411bb61e824aab255fc8
saurabh chauhan <sc42207@gmail.com>
2020-02-06 16:27:05 +0530

    Update patter-24.py

--- patter-24.py
+++ patter-24.py
@@ -1,7 +1,7 @@
-n=9
-for i in range(n//2,-1,-1):
+n1=9
+for i in range(n1//2,-1,-1):
     z=1
-    for j in range(0,n//2+1):
+    for j in range(0,n1//2+1):
         if(j<i):
             print(" ",end='')
         else:
@@ -9,7 +9,7 @@ for i in range(n//2,-1,-1):
             z+=1
     print()
 
-for i in range(0,n//2):
-    for j in range(0,n//2+1):
+for i in range(0,n1//2):
+    for j in range(0,n1//2+1):
         print(j+1,end=" ") if j>i else print(" ",end='') 
     print()       
